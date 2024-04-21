cd070a43aad9cee21b47b52493bab359661f5ffb
Yassine Laababi <yassinelaababi05@gmail.com>
2024-04-21 20:04:44 +0100

    test quotes

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once './src/RandomQuotes.php' ;
+
+$rq= new \RandomQuotes\RandomQuotes ;
+
+echo $rq->generate() ;
+
+
+
+
+
+
+?>
---
+++ src/RandomQuotes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RandomQuotes ;
+
+class RandomQuotes {
+    public function generate(){
+        return '<h1>good work</h1>';
+    }
+
+
+}
+
+
+
+
+
+
+
+?>
