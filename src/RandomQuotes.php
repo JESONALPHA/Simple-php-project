31fb1d9b73a833a5d4fcd9e02880f8f861970528
Yassine Laababi <yassinelaababi05@gmail.com>
2024-04-22 23:09:45 +0100

    fix the array converting error

--- src/RandomQuotes.php
+++ src/RandomQuotes.php
@@ -5,18 +5,18 @@ namespace RandomQuotes ;
 
 class RandomQuotes {
 //create the public function that import the json file
-    public function getRandomQuotes(){ 
+    public function getRandomQuotes(){
 
-//inside the function get the list of Quotes
-  $quotes= file_get_contents(__DIR__ . '/../inc/quotes.json') ;
+        //inside the function get the list of Quotes
+        $quotes= file_get_contents( '/../inc/quotes.json' ) ;
 
-//convert the json file to a php array
-  $quotes = json_decode($quotes ,true);
-//get a random index number
+        //convert the json file to a php array
+        $quotes = json_decode($quotes ,true);
+        //get a random index number
 
-  $index = mt_rand(0 ,count($quotes));
+        $index = mt_rand(0 ,count($quotes));
 
-  return $quotes[$index];
+        return $quotes[$index];
 }
 
 
